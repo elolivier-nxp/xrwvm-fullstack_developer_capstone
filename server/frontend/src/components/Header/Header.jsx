6a992e60321a1099d315fc909b2d32352f356d87
Ernesto Olivier <ernesto.olivier@nexplore.com>
2024-02-18 16:04:20 +0100

    add login

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -9,58 +9,58 @@ const Header = () => {
     const res = await fetch(logout_url, {
       method: "GET",
     });
-  
+
     const json = await res.json();
-    if (json) {
-      let username = sessionStorage.getItem('username');
-      sessionStorage.removeItem('username');
-      window.location.href = window.location.origin;
-      window.location.reload();
-      alert("Logging out "+username+"...")
-    }
-    else {
-      alert("The user could not be logged out.")
-    }
-  };
-    
-//The default home page items are the login details panel
-let home_page_items =  <div></div>
+        if (json) {
+          let username = sessionStorage.getItem('username');
+          sessionStorage.removeItem('username');
+          window.location.href = window.location.origin;
+          window.location.reload();
+          alert("Logging out "+username+"...")
+        }
+        else {
+          alert("The user could not be logged out.")
+        }
+    };
 
-//Gets the username in the current session
-let curr_user = sessionStorage.getItem('username')
+    //The default home page items are the login details panel
+    let home_page_items =  <div></div>
 
-//If the user is logged in, show the username and logout option on home page
-if ( curr_user !== null &&  curr_user !== "") {
-    home_page_items = <div className="input_panel">
-      <text className='username'>{sessionStorage.getItem("username")}</text>
-    <a className="nav_item" href="/djangoapp/logout" onClick={logout}>Logout</a>
-  </div>
-}
+    //Gets the username in the current session
+    let curr_user = sessionStorage.getItem('username')
+
+    //If the user is logged in, show the username and logout option on home page
+    if ( curr_user !== null &&  curr_user !== "") {
+        home_page_items = <div className="input_panel">
+          <text className='username' style={{color:"whitesmoke"}}>{sessionStorage.getItem("username")}</text>
+        <a className="nav_item" style={{color:"whitesmoke"}} href="/djangoapp/logout" onClick={logout}>Logout</a>
+      </div>
+    }
     return (
         <div>
-          <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
-            <div class="container-fluid">
+          <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkblue",height:"1in",color:"whitesmoke"}}>
+            <div className="container-fluid">
               <h2 style={{paddingRight: "5%"}}>Dealerships</h2>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href="/">Home</a>
+              <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" style={{fontSize: "larger",color: "greenyellow"}} aria-current="page" href="/">Home</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/about">About Us</a>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{fontSize: "larger",color: "whitesmoke"}} href="/about">About Us</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/contact">Contact Us</a>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{fontSize: "larger",color: "whitesmoke"}} href="/contact">Contact Us</a>
                   </li>
                 </ul>
-                <span class="navbar-text">
-                  <div class="loginlink" id="loginlogout">
+                <span className="navbar-text">
+                  <div className="loginlink" id="loginlogout">
                   {home_page_items}
                   </div>
-                  </span>
+                </span>
               </div>
             </div>
           </nav>
